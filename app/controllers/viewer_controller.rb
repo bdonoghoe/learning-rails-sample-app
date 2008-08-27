bac74ea0d1bad61600afc7008c632a9afa518cf2
Chris Schmitt <chrisjschmitt@hotmail.com>
2008-08-27 00:14:47 -0400

    The subpages were not sorting in accordance with the numeric value of the Position field in the Pages table.  I updated line 5 of ViewerController to retrieve the subpages in order of the Position. Ignore the changes to any other files

--- app/controllers/viewer_controller.rb
+++ app/controllers/viewer_controller.rb
@@ -2,7 +2,7 @@ class ViewerController < ApplicationController
   
   def show
     @page = Page.find_by_name(params[:name])
-    @subpages = @page.subpages
+    @subpages = @page.subpages.find(:all, :order => 'position' )
     @pagetitle = @page.title
     login_required if @page.admin?
   end
